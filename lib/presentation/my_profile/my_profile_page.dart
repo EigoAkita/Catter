b4a21d89bc4619e67f018b1ebfc4c9b6d6108aa2
英吾 <mono.eigo@gmail.com>
2021-06-23 23:31:12 +0900

    #55 不要import削除

--- lib/presentation/my_profile/my_profile_page.dart
+++ lib/presentation/my_profile/my_profile_page.dart
@@ -5,7 +5,6 @@ import 'package:catter_app/presentation/my_post_screen/my_post_screen_page.dart'
 import 'package:catter_app/presentation/my_profile/my_profile_model.dart';
 import 'package:catter_app/presentation/my_profile/widgets/transition_button.dart';
 import 'package:catter_app/presentation/my_profile_change/my_profile_change_page.dart';
-import 'package:catter_app/presentation/setting/setting_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
